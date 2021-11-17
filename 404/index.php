03c3890dbe1b06a2da4868a124ef0600b906f6e4
Marina Velasco <79833409+BambiDesOceans@users.noreply.github.com>
2021-11-17 08:58:21 +0100

    Update index.php

--- 404/index.php
+++ 404/index.php
@@ -7,16 +7,18 @@
     <title>Erreur 404</title>
     <link rel="stylesheet" href="style.css">
       <!-- montserrat -->
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&display=swap" rel="stylesheet">
     <!-- overpass -->
     <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@600&display=swap" rel="stylesheet">
 </head>
 <body> 
+
     <main>
-    <h1>Error 404</h1>
-    <h2>This is not a Trend</h2>
-    <a href="../accueil"><h2>back to trend</h2></a>
+      <h1>Error 404</h1>
+      <h2 id="notTrend">This is not a Trend</h2>
+      <br>
+      <a href="../accueil"><h2>Back to trend</h2></a>
     </main>
-    
+
 </body>
 </html>
