ee07da69d56793238d22c74c05e8179f3d5d8033
Marina Velasco <79833409+BambiDesOceans@users.noreply.github.com>
2021-11-17 13:41:38 +0100

    Add files via upload

---
+++ home/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../Burger/style.css">
+    <!-- Montserrat font -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&display=swap" rel="stylesheet">
+    <!-- Overpass font -->
+    <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@600&display=swap" rel="stylesheet">
+    <title>Home</title>
+</head>
+<body>
+    <h1 class="logoProvisoire"> <span id="T">T</span>ren<span id="d">d</span> </h1>
+
+    <div class="lasTrends"></div>
+    <div class="archives"></div>
+
+
+    
+<!-- <?php
+    include "../Burger/index.php";
+?> -->
+
+    <script src="../Burger/java.js"></script>
+</body>
+</html>
