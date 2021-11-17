aa7f418c025f1eb7d9388b73421a3c8b64aa4385
DOMESDAY <m.andriamiraho@gmail.com>
2021-11-17 18:16:59 +0100

    rajout du error connexion

--- connexion/index.php
+++ connexion/index.php
@@ -18,12 +18,12 @@
 
 <body>
     <?php
-            if($_SERVER['SERVER_NAME']=="localhost"){
-                require '../db_connect/local.php';
-            }else{
-                require '../db_connect/server.php';
+        if($_SERVER['SERVER_NAME']=="localhost"){
+            require '../db_connect/local.php';
+        }else{
+            require '../db_connect/server.php';
             }
-            ?>
+    ?>
     <form method="POST">
         <span class="content_bj">
             <h1 class=bjPseudo>Hello &nbsp</h1>
@@ -32,33 +32,42 @@
         <img src="../img/profil.svg" alt="" class="profil">
         <input type="text" name="pseudo"  placeholder="Pseudo" class="pseudo" required="require">
         <input type="password" name="mdp"  placeholder="Mot de passe" class="mdp" required="require"  id="current-password">
+        <span class="message"></span><br>
         <button type="submit" name="sub">Se connecter</button>
     </form>
     <?php
-        $pseudo=$_POST["pseudo"];
-        $mdp=$_POST["mdp"];
-        $sql_verif="SELECT `login`,`mdp` FROM `utilisateurs` WHERE `login`='$login'";
+        if (isset($_GET["error"])){
+            echo "<script>document.querySelector('span.message').textContent='Your identifiers are not correct 🤔'</script>";
+        }
+        @$pseudo=$_POST["pseudo"];
+        $pseudo=strtolower($pseudo);
+        @$mdp=$_POST["mdp"];
+        //appel de la fonction control
+        $sql_verif="SELECT `pseudo`,`mdp` FROM `utilisateurs` WHERE `pseudo`='$pseudo'";
         if (isset($_POST["sub"])){
             $req_count=$link->query($sql_verif);
             $verif=$req_count->rowCount();
             if ($verif==1){
                 $data_verif=$req_count->fetch(PDO::FETCH_ASSOC);
-                if (password_verify($mdp,$data_verif["mot_de_passe"] ) ){
+                if (password_verify($mdp,$data_verif["mdp"] ) ){
                     session_start();
                     $_SESSION["pseudo"]=$pseudo;
-                    header('Location: ./home');
+                    header('Location: ../home');
                 
                 }else{
-                    header('Location: ./login.php?error=true');
-        
+                    echo "test";
+                    header('Location: ../connexion?error=true');
+                    
                 }
             }else{
-                header('Location: ./login.php?error=true');
+                echo "test2";
+                header('Location: ../connexion?error=true');
             }
         }
     
 
     ?>
+    <article>No account ?<a href="../inscription"> <b>Sign up now</b></a></article>
     <script src="script.js"></script>
 </body>
 
