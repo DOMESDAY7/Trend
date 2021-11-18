7d2a50f8179c98ede1f71d3725f58f0e5a021843
DOMESDAY <m.andriamiraho@gmail.com>
2021-11-18 11:52:32 +0100

    rajout de table et création de l'admin

--- trends.sql
+++ trends.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 16 nov. 2021 à 21:27
+-- Généré le : jeu. 18 nov. 2021 à 11:51
 -- Version du serveur : 10.4.21-MariaDB
--- Version de PHP : 8.0.12
+-- Version de PHP : 7.3.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,10 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `billet` (
   `id_billet` int(11) NOT NULL,
+  `titre` varchar(100) NOT NULL,
   `article` text NOT NULL,
   `id_image` int(11) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  `short_description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `billet`
+--
+
+INSERT INTO `billet` (`id_billet`, `titre`, `article`, `id_image`, `date`, `short_description`) VALUES
+(1, 'Lorem ipsum dolor sit amet, consectetur ad', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"', 2, '2021-11-18', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
+(2, 'Lorem ipsum dolor sit amet, consectetur ad', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"', 3, '2021-11-16', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
+(3, '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"', 0, '2021-11-20', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit');
 
 -- --------------------------------------------------------
 
@@ -58,8 +69,16 @@ CREATE TABLE `utilisateurs` (
   `id_utilisateur` int(11) NOT NULL,
   `pseudo` varchar(20) NOT NULL,
   `login` varchar(100) NOT NULL,
-  `mdp` int(255) NOT NULL
+  `mdp` varchar(255) NOT NULL,
+  `user_check` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id_utilisateur`, `pseudo`, `login`, `mdp`, `user_check`) VALUES
+(14, 'admin', 'test@test', '$2y$10$4Xk74GI1soASe1mU0yXs3ObSn5./yCgfPo0YrhHCrmjNBTVMb8ltG', 0);
 
 --
 -- Index pour les tables déchargées
@@ -91,7 +110,7 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `billet`
 --
 ALTER TABLE `billet`
-  MODIFY `id_billet` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_billet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `commentaire`
@@ -103,7 +122,7 @@ ALTER TABLE `commentaire`
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
