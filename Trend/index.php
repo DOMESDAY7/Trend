e0e872cc45fa58126e30950f32f01d4cc34bf35c
Marina <velascomarina801@outlook.fr>
2021-11-20 17:26:34 +0100

    trend page

--- Trend/index.php
+++ Trend/index.php
@@ -18,16 +18,32 @@
     <?php require '../db_connect/detetction.php';?>
 
     <div class="containerTrend">
+
+        <!-- Infos -->
         <div class="infos"></div>
+
+        <!-- Image -->
         <div class="imgtrend2">
             <img src="../img/pc2.jpg" alt="">
         </div>
+
+        <!-- Texte -->
         <div class="texTrend">
             <p class="texts">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptatem mollitia delectus, accusantium ipsa, quidem, vitae magni laboriosam ullam explicabo aspernatur! Nobis voluptates aliquam odio accusamus, aliquid mollitia facere nulla?</p>
             <p class="date"> 23.oct.2021 </p>
         </div>
+
+        <!-- Boutons utilitaires -->
         <div class="utilities">
-            <div class="SHcomments"></div>
+            <div class="SHcomments">
+                <div class="svg">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M21.257 10.962C21.731 11.582 21.731 12.419 21.257 13.038C19.764 14.987 16.182 19 12 19C7.81801 19 4.23601 14.987 2.74301 13.038C2.51239 12.7411 2.38721 12.3759 2.38721 12C2.38721 11.6241 2.51239 11.2589 2.74301 10.962C4.23601 9.013 7.81801 5 12 5C16.182 5 19.764 9.013 21.257 10.962V10.962Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </div>
+                <p> Show comments </p>
+            </div>
             <div class="putComment"></div>
         </div>
     </div>
