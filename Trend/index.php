5c60a84ec5967378fea1c819da651665bda917f4
Marina <velascomarina801@outlook.fr>
2021-11-20 11:52:36 +0100

    création page "Trend"

---
+++ Trend/index.php
@@ -0,0 +1,32 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- CSS -->
+        <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../component/Burger/style.css">
+    <!-- Montserrat font -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600700&display=swap" rel="stylesheet">
+    <!-- Overpass font -->
+    <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@600;700&display=swap" rel="stylesheet">
+    <title><!-- Mettre le nom du trend en PHP ici --></title>
+</head>
+<body>
+    <?php require '../db_connect/detetction.php';?>
+
+    <main>
+
+    </main>
+    
+
+
+<?php
+    include "../component/Burger/index.php";
+?>
+
+    <script src="../component/Burger/java.js"></script>
+</body>
+</html>
