0e44387aea87b6256b2f4eb0d669af3d08adda75
DOMESDAY <m.andriamiraho@gmail.com>
2021-11-21 12:24:51 +0100

    début de page de back

---
+++ backoffice/menu/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Back office reception</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- Montserrat font -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&display=swap" rel="stylesheet">
+    <!-- Overpass font -->
+    <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@100;300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <main>
+        
+        <span class="content_addTrend"><a href=""><span class="hashtag">#</span> Add a trend</a></span>
+        <span class="line trend_modify">
+        <a href=""><span class="hashtag">#</span> Modify a trend</a>
+        </span>
+        <section class="content_manage line">
+            <a href=""><span class="hashtag">#</span> Manage comments</a>
+            <a href=""><span class="hashtag">#</span> Manage users</a>
+        </section>
+    </main>
+    
+</body>
+</html>
