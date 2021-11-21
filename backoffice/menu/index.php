391f75ad76439e318359978cadebf97108a6bdd9
DOMESDAY <m.andriamiraho@gmail.com>
2021-11-21 17:26:55 +0100

    panneau de bord backoffice

--- backoffice/menu/index.php
+++ backoffice/menu/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,23 +8,59 @@
     <title>Back office reception</title>
     <link rel="stylesheet" href="style.css">
     <!-- Montserrat font -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&display=swap"
+        rel="stylesheet">
     <!-- Overpass font -->
-    <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@100;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@100;300;400;500;600;700&display=swap"
+        rel="stylesheet">
 </head>
-<body>
 
+<body>
+    <?php
+     require '../../db_connect/detetction.php';
+     $sql="SELECT * FROM utilisateurs , billet ";
+     $req=$link->query($sql);
+    ?>
     <main>
-        
-        <span class="content_addTrend"><a href=""><span class="hashtag">#</span> Add a trend</a></span>
-        <span class="line trend_modify">
+
+        <a href=""><span class="hashtag">#</span> Add a trend</a>
         <a href=""><span class="hashtag">#</span> Modify a trend</a>
-        </span>
-        <section class="content_manage line">
-            <a href=""><span class="hashtag">#</span> Manage comments</a>
-            <a href=""><span class="hashtag">#</span> Manage users</a>
-        </section>
+        <a href=""><span class="hashtag">#</span> Manage comments</a>
+        <a href=""><span class="hashtag">#</span> Manage users</a>
+
     </main>
-    
+    <?php $sql="SELECT pseudo login" ?>
+    <h3 class="title_trend">Last trend</h3>
+    <section class="last_trend">
+        <?php while ($data_trend = $req->fetch()){ ?>
+        <a href="" class="link_trend">
+            <span class="trend">
+            <h3 class="titre"><span class="hashtag">#</span>
+                This is a trend</h3>
+                <h5 class="date">01.dec.2021</h5>
+                <h5 class="nb_comments">number of comments:</h5>
+                <h5 class="nb">10</h5>
+                
+                  <img src="../../img/pc.jpg" alt="">
+            </span>
+        </a>
+        <?php } ?>
+    </section>
+
+    <h3 class="title_user"> Last user</h3>
+    <section class="last_user">
+        <?php while ($data_user = $req->fetch()){ ?>
+        <a href="" class="link_user">
+            <span class="user">
+                <h3 class="pseudo">User</h3>
+                <h3 class="pseudo">test@test</h3>
+                <h5 class="since"> Active since 10.dec.2021</h5>
+                <div class="user_img"></div>
+            </span>
+        </a>
+        <?php } ?>
+
+    </section>
 </body>
+
 </html>
