79089a8b5a3e2f052d6070ef55c9b6b7633b03d0
Marina Velasco <79833409+BambiDesOceans@users.noreply.github.com>
2021-11-22 17:34:11 +0100

    Add files via upload

---
+++ Trend/script.js
@@ -0,0 +1,38 @@
+// Show/Hide comments
+
+let areaComments = document.querySelector('.containerComments');
+let eye = document.querySelector('.eye');
+let p = document.querySelector('.p');
+let show = "Show comments";
+let hide = "Hide comments";
+
+document.querySelector('.SHcomments').addEventListener('click', ()=>{
+    areaComments.classList.toggle('show');
+
+    if(areaComments.classList.contains('show') ==  true){
+        console.log('its showing');
+
+        eye.src = "../img/eyeClosed.svg";
+        p.textContent = hide;
+    }
+    
+    if(areaComments.classList.contains('show') == false){
+        console.log('its hidden');
+
+        eye.src = "../img/eyeOpen.svg"
+        p.textContent = show;
+    }
+});
+
+
+// Modal
+let BG2 = document.querySelector('.BGmodal');
+let croix = document.querySelector('.croix');
+
+document.querySelector('.putComment').addEventListener('click', ()=>{
+    BG2.classList.toggle('display');
+});
+
+croix.addEventListener('click', ()=>{
+    BG2.classList.toggle('display');
+})
