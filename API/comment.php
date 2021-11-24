f2d0079b9ee193c132b15478fe1576e9fa49e7ca
DOMESDAY7 <79838340+DOMESDAY7@users.noreply.github.com>
2021-11-24 21:27:04 +0100

    réfléxion sur l'envoie de donnée en fetch

--- API/comment.php
+++ API/comment.php
@@ -1,20 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>API commentaire</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php
-    require '../db_connect/detetction.php';
-    $sql="SELECT * FROM commentaire";
-    $req=$link->query($sql);
-    while($data=$req->fetch(PDO::FETCH_ASSOC)){
-        echo(json_encode($data));
-    }
-    ?>  
+    <form method="POST">
+        <input type="text" name="" id="">
+        <input type="submit" value="test">
+    </form>
 </body>
 </html>
+
+<?php
+//   header('Content-Type: application/json');
+  header("Access-Control-Allow-Origin: * ");
+  header('Access-Control-Allow-Headers: Content-Type');
+  header("Access-Control-Request-Headers: *");
+    require '../db_connect/detetction.php';
+    
+   
+    var_dump($_POST);
+    $content=$_POST["content"];
+    $post_date=$data["post_date"];
+    $ext_billet=$data["ext_billet"];
+    $ext_utilisateur=$data["ext_utilisateur"];
+    $sql="INSERT INTO `commentaire` (`id_commentaire`, `post_date`, `content`, `ext_billet`, `ext_utilisateur`) VALUES (NULL, '$post_date', '$content', '$ext_billet', '$ext_utilisateur')";
+    //peut être faire une requête préparé pour avec des filter_var
+    $link->query($sql);
+
+
+
+    
+
+
+
+
+
+    
+
+    // $content = trim(file_get_contents("php://input"));
+
+    // $data = json_decode($content, true);
+    // $data['success'] = true;
+    
+    // echo json_encode($data);
+
+
+
+
+    // $sql="SELECT * FROM commentaire";
+    // $req=$link->query($sql);
+    // while($data=$req->fetch(PDO::FETCH_ASSOC)){
+    //     echo(json_encode($data));
+    // }
+?>
