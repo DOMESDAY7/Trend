f26b29a77e5797a384922c103f8ce1e49d05f9cd
DOMESDAY7 <79838340+DOMESDAY7@users.noreply.github.com>
2021-11-25 10:15:29 +0100

    petit changement

--- home/model.php
+++ home/model.php
@@ -2,7 +2,7 @@
 <?php function lastThree(){?>
     <?php 
     require '../db_connect/detetction.php';
-     $sql="SELECT short_description , id_image , date , titre  FROM billet ORDER BY date DESC ";
+     $sql="SELECT short_description , id_image , date , titre, id_billet  FROM billet ORDER BY date DESC ";
      $req=$link -> query($sql);
      $cpt=0;
     
@@ -12,17 +12,19 @@
         ?>
         <?php $date=date_create($data["date"]) ?>
         
-        <div class="exTrend1">
-            <div class="textsTrend">
-            <!-- pour le hashtag on va mettre le lien de l'article -->
-                <h1><span id="hashtag">#</span> <?php echo $data["titre"]; ?> </h1> 
-                <p class="description"> <?php echo $data["short_description"]; ?></p>
-                <p class="date"><?php echo date_format($date,'d.M.Y') ; ?></p>
+        <a href="<?php echo $data["id_billet"]; ?>">
+            <div class="exTrend1">
+                <div class="textsTrend">
+                <!-- pour le hashtag on va mettre le lien de l'article -->
+                    <h1><span id="hashtag">#</span> <?php echo $data["titre"]; ?> </h1>
+                    <p class="description"> <?php echo $data["short_description"]; ?></p>
+                    <p class="date"><?php echo date_format($date,'d.M.Y') ; ?></p>
+                </div>
+                <div class="imgTrend">
+                    <img src="../img/pc.jpg" alt="">
+                </div>
             </div>
-            <div class="imgTrend">
-                <img src="../img/pc.jpg" alt="">
-            </div>
-        </div>
+        </a>
     <?php $cpt++; 
     }
 }
