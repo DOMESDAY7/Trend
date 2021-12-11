c707e2227f16e1cf865d3ed92df6d0c375500c5d
Marina <velascomarina801@outlook.fr>
2021-12-11 22:34:58 +0100

    c'est réparé zebi

--- model/home.php
+++ model/home.php
@@ -5,26 +5,31 @@ function lastThree()
     $sql = "SELECT short_description , id_image , date , titre, id_billet  FROM billet ORDER BY date DESC ";
     $req = $link->query($sql);
     $cpt = 0;
-
-    while (($data = $req->fetch(PDO::FETCH_ASSOC)) && ($cpt < 3)) {
+    ?>
+        <div  class="containerLT">
+    <?php while (($data = $req->fetch(PDO::FETCH_ASSOC)) && ($cpt < 3)) {
 
 
 ?>
         <?php $date = date_create($data["date"]) ?>
 
-        <a href="?page=trend&id_billet=<?= $data["id_billet"]; ?>">
-            <div class="exTrend1">
-                <div class="textsTrend">
-                    <!-- pour le hashtag on va mettre le lien de l'article -->
-                    <h1><span id="hashtag">#</span> <?= $data["titre"]; ?> </h1>
-                    <p class="description"> <?= $data["short_description"]; ?></p>
-                    <p class="date"><?= date_format($date, 'd.M.Y'); ?></p>
+
+            <a href="?page=trend&id_billet=<?= $data["id_billet"]; ?>" class="linkContainerTrend">
+                <div class="contentTrend">
+                    <div class="textsTrend">
+                        <!-- pour le hashtag on va mettre le lien de l'article -->
+                        <h1><span id="hashtag">#</span> <?= $data["titre"]; ?> </h1>
+                        <p class="description"> <?= $data["short_description"]; ?></p>
+                        <p class="date"><?= date_format($date, 'd.M.Y'); ?></p>
+                    </div>
+                    <div class="imgTrend">
+                        <img src="./public/img/pc.jpg" alt="">
+                    </div>
                 </div>
-                <div class="imgTrend">
-                    <img src="./public/img/pc.jpg" alt="">
-                </div>
-            </div>
-        </a>
+            </a>
+
 <?php $cpt++;
     }
-}
+           
+}?>
+</div>
