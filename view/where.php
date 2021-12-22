e9aaeec8d1a42e36a657728de71bd2f33ae00223
Marina Velasco <79833409+BambiDesOceans@users.noreply.github.com>
2021-12-22 17:47:41 +0100

    Add files via upload

---
+++ view/where.php
@@ -0,0 +1,44 @@
+<body>
+    <main>
+        <section class="enter">
+            <h1>Where am I ?</h1>
+            <div class="explications">
+                <p id="p1"><span class="red">Hello !</span> And welcome in Trend, a blog created by two MMI students.</p>
+                <br>
+                <p id="p2">The principe is simple. Sometimes, a Trend is added by us, and you, user, you can see it and comment it. But careful ! You can add a comment only if you're connected with an account !</p>
+            </div>
+        </section>
+        <section class="owners">
+            <h2>The owners</h2>
+            <div class="us">
+                <div class="card mathieu">
+                    <div class="pp ppMathieu"></div>
+                    <h3>Mathieu</h3>
+                    <!-- <p>Développeur Back</p> -->
+                    <div class="reseaux">
+                        <a href="#" target="_blank"><img src="../public/img/linkedIn.png" alt="" class="linkedin"></a>
+                        <a href="#" target="_blank"><img src="../public/img/git.png" alt="" class="git"></a>
+                    </div>
+                </div>
+                <div class="card marina">
+                    <div class="pp ppMarina"></div>
+                    <h3>Marina</h3>
+                    <!-- <p>Développeuse Front</p> -->
+                    <div class="reseaux">
+                        <a href="https://www.linkedin.com/in/marina-velasco-7690b3201/" target="_blank"><img src="../public/img/linkedIn.png" alt="" class="linkedin"></a>
+                        <a href="https://www.instagram.com/eso.hia/" target="_blank"><img src="../public/img/insta.png" alt="" class="insta"></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    
+
+
+<?php
+    require './view/components/Burger/index.php';
+?>
+
+<script src="./view/components/Burger/java.js"></script>
+</body>
+</html>
