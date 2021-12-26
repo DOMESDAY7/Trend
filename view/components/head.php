e31ae96ad0c628ae007d1ae95be14594a8c5f0b7
Marina <velascomarina801@outlook.fr>
2021-12-26 17:01:50 +0100

    logoFavicon

--- view/components/head.php
+++ view/components/head.php
@@ -12,6 +12,7 @@
                                                     echo $page;
                                                 } ?>.css">
     <link rel="stylesheet" href="./view/components/Burger/style.css">
+    <link rel="shortcut icon" href="././public/img/logo.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@600&display=swap" rel="stylesheet">
